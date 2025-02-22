0e0e2fa0ed7fc7ba9ebdcaa73f6e52f276bfc1be
Shuqian Xu <xshuqian@gmail.com>
2025-02-22 19:07:58 +0100

    All 200 human annotations complete

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import scripts
 
 if __name__ == "__main__":
+    # Check if all files in the human annotations folder exist in the both LLM folders
     human_folder = "data/post_validation_human_annotations"
-
-    # Call the function using the full module path
     scripts.file_loader.check_missing_files(human_folder, "data/gemma_2-9b_output", "gemma_2-9b_output")
     scripts.file_loader.check_missing_files(human_folder, "data/llama_3.1-8b_output", "llama_3.1-8b_output")
